fea635046ee9b4029482786dcab4232d129bcdfb
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 11:57:51 -0300

    feat: create Coupon entity with attributes and JPA annotations

---
+++ src/main/java/com/vsantos/springtechevents/domain/coupon/Coupon.java
@@ -0,0 +1,38 @@
+package com.vsantos.springtechevents.domain.coupon;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.vsantos.springtechevents.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  private String code;
+  private Integer discount;
+  private Date expirationDate;
+
+  @ManyToOne
+  @JoinColumn(name = "event_id")
+  private Event event;
+}
