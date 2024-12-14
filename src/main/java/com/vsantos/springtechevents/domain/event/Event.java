076b2febcd4baaaf89aaa6fe329e371f3dd0e182
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 11:57:45 -0300

    feat: create Event entity with attributes and JPA annotations

---
+++ src/main/java/com/vsantos/springtechevents/domain/event/Event.java
@@ -0,0 +1,33 @@
+package com.vsantos.springtechevents.domain.event;
+
+import java.util.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "event")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  private String title;
+  private String description;
+  private String imgUrl;
+  private String eventUrl;
+  private Boolean remote;
+  private Date date;
+}
