d0770237a3875c3a341083cfd5b08e21f94ebe58
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 11:57:48 -0300

    feat: create Address entity with attributes and JPA annotations

---
+++ src/main/java/com/vsantos/springtechevents/domain/address/Address.java
@@ -0,0 +1,35 @@
+package com.vsantos.springtechevents.domain.address;
+
+import java.util.UUID;
+
+import com.vsantos.springtechevents.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  private String city;
+  private String uf;
+
+  @ManyToOne
+  @JoinColumn(name = "event_id")
+  private Event event;
+}
