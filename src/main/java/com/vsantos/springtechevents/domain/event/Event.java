5954e3af73ce331f1b06deef3e6b4936febad298
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 15:37:54 -0300

    feat: add Lombok @Builder annotation to Event entity for improved object creation

--- src/main/java/com/vsantos/springtechevents/domain/event/Event.java
+++ src/main/java/com/vsantos/springtechevents/domain/event/Event.java
@@ -8,12 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "event")
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
