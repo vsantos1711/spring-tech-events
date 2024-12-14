9b9e7af841ca61235cc0a8917a8eb1695833239a
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 11:05:28 -0300

    chore: initial commit

---
+++ src/main/java/com/vsantos/springtechevents/SpringTechEventsApplication.java
@@ -0,0 +1,13 @@
+package com.vsantos.springtechevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTechEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsantos/springtechevents/SpringTechEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsantos.springtechevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTechEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
