4b9aa396e3aa3c12c00de84c83e58585a110fe91
vsantos1711 <vsantos067100@gmail.com>
2024-12-14 20:48:52 -0300

    feat: add CouponController for creating coupons associated with events

---
+++ src/main/java/com/vsantos/springtechevents/controller/CouponController.java
@@ -0,0 +1,30 @@
+package com.vsantos.springtechevents.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vsantos.springtechevents.domain.coupon.Coupon;
+import com.vsantos.springtechevents.domain.coupon.CouponRequestDTO;
+import com.vsantos.springtechevents.services.CouponService;
+
+@RestController
+@RequestMapping("/coupon")
+public class CouponController {
+
+  @Autowired
+  private CouponService couponService;
+
+  @PostMapping("/event/{eventId}")
+  public ResponseEntity<Coupon> createCoupon(@PathVariable UUID eventId, @RequestBody CouponRequestDTO couponDTO) {
+    Coupon coupon = couponService.createCoupon(eventId, couponDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body(coupon);
+  }
+}
