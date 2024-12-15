fcd5fa0919e7f6e897c73002ee22c28911d9536d
vsantos1711 <vsantos067100@gmail.com>
2024-12-15 12:21:03 -0300

    feat: add method to find upcoming events in EventRepository

--- src/main/java/com/vsantos/springtechevents/repositories/EventRepository.java
+++ src/main/java/com/vsantos/springtechevents/repositories/EventRepository.java
@@ -1,8 +1,13 @@
 package com.vsantos.springtechevents.repositories;
 
+import java.sql.Date;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vsantos.springtechevents.domain.event.Event;
@@ -10,4 +15,6 @@ import com.vsantos.springtechevents.domain.event.Event;
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
+  @Query("SELECT e FROM Event e WHERE e.date >= CURRENT_DATE")
+  public Page<Event> findUpcomingEvents(@Param("currentDate") Date currentDate, Pageable pageable);
 }
