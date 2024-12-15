d8f01523a1618b5d06ad811e96418d8b0514a9a4
vsantos1711 <vsantos067100@gmail.com>
2024-12-15 12:20:58 -0300

    feat: update EventResponseDTO to use 'uf' instead of 'state' for improved clarity

--- src/main/java/com/vsantos/springtechevents/domain/event/EventResponseDTO.java
+++ src/main/java/com/vsantos/springtechevents/domain/event/EventResponseDTO.java
@@ -6,6 +6,6 @@ import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-public record EventResponseDTO(UUID id, String title, String description, Date date, String city, String state,
-                Boolean remote, String eventUrl, String imgUrl) {
+public record EventResponseDTO(UUID id, String title, String description, Date date, String city, String uf,
+    Boolean remote, String eventUrl, String imgUrl) {
 }
