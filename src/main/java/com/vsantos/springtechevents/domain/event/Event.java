a26926b5533dc7a1cd045ec5fd063bebbb291754
vsantos1711 <vsantos067100@gmail.com>
2024-12-15 12:20:47 -0300

    feat: add one-to-one relationship between Event and Address entities

--- src/main/java/com/vsantos/springtechevents/domain/event/Event.java
+++ src/main/java/com/vsantos/springtechevents/domain/event/Event.java
@@ -3,9 +3,13 @@ package com.vsantos.springtechevents.domain.event;
 import java.util.Date;
 import java.util.UUID;
 
+import com.vsantos.springtechevents.domain.address.Address;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,4 +36,7 @@ public class Event {
   private String eventUrl;
   private Boolean remote;
   private Date date;
+
+  @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+  private Address address;
 }
