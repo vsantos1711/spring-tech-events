eaec3818b96cdad08a14cc28efe2cecf120a9bff
vsantos1711 <vsantos067100@gmail.com>
2024-12-15 12:20:21 -0300

    feat: update getEvents method to retrieve upcoming events

--- src/main/java/com/vsantos/springtechevents/controller/EventController.java
+++ src/main/java/com/vsantos/springtechevents/controller/EventController.java
@@ -39,7 +39,7 @@ public class EventController {
   @GetMapping
   public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page,
       @RequestParam(defaultValue = "10") int size) {
-    List<EventResponseDTO> allEvents = eventService.getEvents(page, size);
+    List<EventResponseDTO> allEvents = eventService.getUpcomingEvents(page, size);
     return ResponseEntity.status(HttpStatus.OK).body(allEvents);
   }
 }
