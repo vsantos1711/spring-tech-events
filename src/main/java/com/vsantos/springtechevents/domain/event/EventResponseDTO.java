595c43524b9100942edce40035a4a851b487adb1
vsantos1711 <vsantos067100@gmail.com>
2024-12-16 16:53:20 -0300

    feat: update EventResponseDTO to use LocalDateTime for event date

--- src/main/java/com/vsantos/springtechevents/domain/event/EventResponseDTO.java
+++ src/main/java/com/vsantos/springtechevents/domain/event/EventResponseDTO.java
@@ -1,11 +1,11 @@
 package com.vsantos.springtechevents.domain.event;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import lombok.Builder;
 
 @Builder
-public record EventResponseDTO(UUID id, String title, String description, Date date, String city, String uf,
-    Boolean remote, String eventUrl, String imgUrl) {
+public record EventResponseDTO(UUID id, String title, String description, LocalDateTime date, String city, String uf,
+        Boolean remote, String eventUrl, String imgUrl) {
 }
