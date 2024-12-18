0bea197891273a86d8c341baf00c1397a3e022b9
vsantos1711 <vsantos067100@gmail.com>
2024-12-18 15:40:01 -0300

    feat: add method to find Coupons by event ID and valid date in CouponRepository

--- src/main/java/com/vsantos/springtechevents/repositories/CouponRepository.java
+++ src/main/java/com/vsantos/springtechevents/repositories/CouponRepository.java
@@ -1,5 +1,7 @@
 package com.vsantos.springtechevents.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,5 @@ import com.vsantos.springtechevents.domain.coupon.Coupon;
 
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
-
+  List<Coupon> findByEventIdAndValidAfter(UUID eventId, LocalDateTime currentDate);
 }
