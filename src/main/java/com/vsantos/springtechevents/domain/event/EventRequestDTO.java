a8c93cb89952bac7395e31b0afa24e01a9358030
vsantos1711 <vsantos067100@gmail.com>
2024-12-31 15:18:21 -0300

    feat: enhance EventRequestDTO validation by adding required constraints for address and event URL

--- src/main/java/com/vsantos/springtechevents/domain/event/EventRequestDTO.java
+++ src/main/java/com/vsantos/springtechevents/domain/event/EventRequestDTO.java
@@ -10,41 +10,40 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
-
 
 @Builder
 public record EventRequestDTO(
 
-    @NotBlank(message = "The title is required")
-    @Schema(description = "Event title", example = "Java Conference")
-    String title,
+        @NotBlank(message = "The title is required")
+        @Schema(description = "Event title", example = "Java Conference")
+        String title,
 
-    @NotBlank(message = "The description is required")
-    @Schema(description = "Event description", example = "The best Java conference in the world")
-    String description,
+        @NotBlank(message = "The description is required")
+        @Schema(description = "Event description", example = "The best Java conference in the world")
+        String description,
 
-    @FutureOrPresent(message = "Event date must be in the present or future")
-    @Schema(description = "Event date", example = "2022-12-01T10:00:00")
-    OffsetDateTime date,
+        @FutureOrPresent(message = "Event date must be in the present or future")
+        @Schema(description = "Event date", example = "2022-12-01T10:00:00")
+        OffsetDateTime date,
 
-    @Schema(description = "Event address", example = "Java Street")
-    String city,
+        @NotBlank(message = "The address is required")
+        @Schema(description = "Event address", example = "Java Street")
+        String city,
 
-    @Size(min = 2, max = 2, message = "State abbreviation must be 2 characters")
-    @Pattern(regexp = "[A-Za-z]{2}", message = "State abbreviation must consist of two letters")
-    @Schema(description = "Event state", example = "SP")
-    String uf,
+        @Pattern(regexp = "[A-Za-z]{2}", message = "State abbreviation must consist of two letters")
+        @Schema(description = "Event state", example = "SP")
+        String uf,
 
-    @NotNull(message = "The remote flag is required")
-    @Schema(description = "Is the event remote?")
-    Boolean remote,
+        @NotNull(message = "The remote flag is required")
+        @Schema(description = "Is the event remote?")
+        Boolean remote,
 
-    @URL(message = "Event URL must be a valid URL")
-    @Schema(description = "Event URL", example = "https://javac.com.br")
-    String eventUrl,
+        @NotBlank(message = "The event URL is required")
+        @URL(message = "Event URL must be a valid URL")
+        @Schema(description = "Event URL", example = "https://javac.com.br")
+        String eventUrl,
 
-    @Schema(description = "Upload an image", type = "string", format = "binary")
-    MultipartFile image) {
+        @Schema(description = "Upload an image", type = "string", format = "binary")
+        MultipartFile image) {
 }
